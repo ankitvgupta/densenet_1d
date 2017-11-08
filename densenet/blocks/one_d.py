7f820a617b7929038a03b2db3635b36be8d6863c
Ankit Gupta <ankitgupta@college.harvard.edu>
2017-11-08 00:51:40 -0800

    ran autopep8

--- densenet/blocks/one_d.py
+++ densenet/blocks/one_d.py
@@ -2,17 +2,30 @@
 from keras.layers import BatchNormalization, Activation, Conv1D, Concatenate, AveragePooling1D
 
 # A single convolutional operation, defined as H_l in the original paper
+
+
 def H_l(k, bottleneck_size, kernel_width):
     use_bottleneck = bottleneck_size > 0
-    num_bottleneck_output_filters = k*bottleneck_size
+    num_bottleneck_output_filters = k * bottleneck_size
+
     def f(x):
         if use_bottleneck:
             x = BatchNormalization()(x)
             x = Activation("relu")(x)
-            x = Conv1D(num_bottleneck_output_filters, 1, strides=1, padding="same", dilation_rate=1)(x)
+            x = Conv1D(
+                num_bottleneck_output_filters,
+                1,
+                strides=1,
+                padding="same",
+                dilation_rate=1)(x)
         x = BatchNormalization()(x)
         x = Activation("relu")(x)
-        x = Conv1D(k, kernel_width, strides=1, padding="same", dilation_rate=1)(x)
+        x = Conv1D(
+            k,
+            kernel_width,
+            strides=1,
+            padding="same",
+            dilation_rate=1)(x)
         return x
     return f
 
@@ -28,14 +41,23 @@ def dense_block(k, num_layers, kernel_width, bottleneck_size):
         return x
     return f
 
+
 def transition_block(pool_size=2, stride=2, theta=0.5):
     assert theta > 0 and theta <= 1
+
     def f(x):
-        num_transition_output_filters = int(int(x.shape[2])*float(theta))
+        num_transition_output_filters = int(int(x.shape[2]) * float(theta))
         x = BatchNormalization()(x)
         x = Activation("relu")(x)
-        x = Conv1D(num_transition_output_filters, 1, strides=1, padding="same", dilation_rate=1)(x)
-        x = AveragePooling1D(pool_size=pool_size, strides=stride, padding="same")(x)
+        x = Conv1D(
+            num_transition_output_filters,
+            1,
+            strides=1,
+            padding="same",
+            dilation_rate=1)(x)
+        x = AveragePooling1D(
+            pool_size=pool_size,
+            strides=stride,
+            padding="same")(x)
         return x
     return f
-
--- densenet/classifiers/one_d.py
+++ densenet/classifiers/one_d.py
@@ -1,11 +1,9 @@
 import keras.models
-from keras.layers import Input, Flatten, Dense
-from keras.models import Model
+from keras.layers import Input, Dense
 import densenet.models.one_d
 
-
 class DenseNet121(keras.models.Model):
-    """ Create a Keras Model Object that is a customized Densenet121 implementation"""
+    """  Create a Keras Model Object that is an implementation of DenseNet121 """
 
     def __init__(
             self,
@@ -36,6 +34,149 @@ class DenseNet121(keras.models.Model):
             initial_pool_width,
             initial_pool_stride,
             use_global_pooling=True)(model_input)
-        #output = Flatten()(output)
         output = Dense(num_outputs, activation="softmax")(output)
         super(DenseNet121, self).__init__(model_input, output)
+
+class DenseNet169(keras.models.Model):
+    """ Create a Keras Model Object that is an implementation of DenseNet169 """
+
+    def __init__(
+            self,
+            input_shape,
+            num_outputs,
+            k,
+            conv_kernel_width,
+            bottleneck_size,
+            transition_pool_size,
+            transition_pool_stride,
+            theta,
+            initial_conv_width,
+            initial_stride,
+            initial_filters,
+            initial_pool_width,
+            initial_pool_stride):
+        model_input = Input(shape=input_shape)
+        output = densenet.models.one_d.DenseNet169(
+            k,
+            conv_kernel_width,
+            bottleneck_size,
+            transition_pool_size,
+            transition_pool_stride,
+            theta,
+            initial_conv_width,
+            initial_stride,
+            initial_filters,
+            initial_pool_width,
+            initial_pool_stride,
+            use_global_pooling=True)(model_input)
+        output = Dense(num_outputs, activation="softmax")(output)
+        super(DenseNet169, self).__init__(model_input, output)
+
+
+class DenseNet201(keras.models.Model):
+    """ Create a Keras Model Object that is an implementation of DenseNet201 """
+
+    def __init__(
+            self,
+            input_shape,
+            num_outputs,
+            k,
+            conv_kernel_width,
+            bottleneck_size,
+            transition_pool_size,
+            transition_pool_stride,
+            theta,
+            initial_conv_width,
+            initial_stride,
+            initial_filters,
+            initial_pool_width,
+            initial_pool_stride):
+        model_input = Input(shape=input_shape)
+        output = densenet.models.one_d.DenseNet201(
+            k,
+            conv_kernel_width,
+            bottleneck_size,
+            transition_pool_size,
+            transition_pool_stride,
+            theta,
+            initial_conv_width,
+            initial_stride,
+            initial_filters,
+            initial_pool_width,
+            initial_pool_stride,
+            use_global_pooling=True)(model_input)
+        output = Dense(num_outputs, activation="softmax")(output)
+        super(DenseNet201, self).__init__(model_input, output)
+
+class DenseNet264(keras.models.Model):
+    """ Create a Keras Model Object that is an implementation of DenseNet264"""
+
+    def __init__(
+            self,
+            input_shape,
+            num_outputs,
+            k,
+            conv_kernel_width,
+            bottleneck_size,
+            transition_pool_size,
+            transition_pool_stride,
+            theta,
+            initial_conv_width,
+            initial_stride,
+            initial_filters,
+            initial_pool_width,
+            initial_pool_stride):
+        model_input = Input(shape=input_shape)
+        output = densenet.models.one_d.DenseNet264(
+            k,
+            conv_kernel_width,
+            bottleneck_size,
+            transition_pool_size,
+            transition_pool_stride,
+            theta,
+            initial_conv_width,
+            initial_stride,
+            initial_filters,
+            initial_pool_width,
+            initial_pool_stride,
+            use_global_pooling=True)(model_input)
+        output = Dense(num_outputs, activation="softmax")(output)
+        super(DenseNet264, self).__init__(model_input, output)
+
+class DenseNetCustom(keras.models.Model):
+    """  Create a Keras Model Object that is an implementation of DenseNet with a custom number of parameters. The number of layers per dense block can be specified by block_sizes."""
+
+    def __init__(
+            self,
+            input_shape,
+            num_outputs,
+            k,
+            block_sizes,
+            conv_kernel_width,
+            bottleneck_size,
+            transition_pool_size,
+            transition_pool_stride,
+            theta,
+            initial_conv_width,
+            initial_stride,
+            initial_filters,
+            initial_pool_width,
+            initial_pool_stride):
+        model_input = Input(shape=input_shape)
+        output = densenet.models.one_d.DenseNet(
+            k,
+            block_sizes,
+            conv_kernel_width,
+            bottleneck_size,
+            transition_pool_size,
+            transition_pool_stride,
+            theta,
+            initial_conv_width,
+            initial_stride,
+            initial_filters,
+            initial_pool_width,
+            initial_pool_stride,
+            use_global_pooling=True)(model_input)
+        output = Dense(num_outputs, activation="softmax")(output)
+        super(DenseNet264, self).__init__(model_input, output)
+
